78cbf86e8a403fa8181085c67e3b8d01de311628
clueless-skywatcher <somichat@gmail.com>
2018-09-10 01:03:16 +0530

    Added minimal docs and 'vars' function

--- kctypes.py
+++ kctypes.py
@@ -1,4 +1,7 @@
 class Add:
+    '''
+    Define a sum with left and right terms
+    '''
     def __init__(self, left, right):
         self.left = left
         self.right = right
@@ -16,6 +19,9 @@ class Add:
             return Add(self, other)
 
 class Product:
+    '''
+    Define a product with left and right terms
+    '''
     def __init__(self, left, right):
         self.left = left
         self.right = right
@@ -35,6 +41,28 @@ class Product:
             return '{0}{1}{2}'.format(str(self.left), '*', str(self.right))
 
 class Var:
+    '''
+    Class for defining a symbol variable
+    Usage:
+    >> x = Var('x')
+    >> x
+    x
+    >> y = Var('x', coeff = 2)
+    >> y
+    2*x
+
+    Arguments:
+        name : Name to be held in the variable symbol
+        coeff : Coefficient of the variable symbol, by default 1
+
+    We strongly recommend against choosing placeholder names with non-unit coefficients
+    to be same as the variable names themselves, i.e we don't recommend using the following type of
+    declarations:
+
+    >> x = Var('x', coeff = 2) --> Avoid doing this
+
+    '''
+
     def __init__(self, name, coeff = 1):
         self.name = name
         self.coeff = coeff
@@ -53,3 +81,36 @@ class Var:
             return self.name
         else:
             return str(self.coeff) + '*' + self.name
+
+    def __eq__(self, other):
+        if isinstance(other, Var):
+            if self.name == other.name:
+                return self.coeff == other.coeff
+            else:
+                return False
+        else:
+            return False
+
+def vars(s):
+    '''
+    Return symbols provided in string s
+    Usage:
+    >> x, y, z = vars('x, y, z')
+    >> u, v, w = vars('2*x, 4*y, z')
+    >> u
+    2*x
+    >> v
+    4*y
+    >> w
+    z
+    '''
+    a = s.split(', ')
+    l = []
+    for i in a:
+        if len(i) == 1:
+            l.append(Var(i))
+        else:
+            co = i.split('*')[0]
+            nm = i.split('*')[1]
+            l.append(Var(nm, co))
+    return tuple(l)
