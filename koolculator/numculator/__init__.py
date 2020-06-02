adaae8b80911342891a5cad38b8b82d488dc1ac6
clueless-skywatcher <28658223+clueless-skywatcher@users.noreply.github.com>
2020-06-02 20:22:35 +0530

    Chinese remainder solver added under modular package

--- koolculator/numculator/__init__.py
+++ koolculator/numculator/__init__.py
@@ -1,3 +1,3 @@
 from koolculator.numculator.sieve import *
 from koolculator.numculator.prime import *
-from koolculator.numculator.euclid import *
+from koolculator.numculator.modulo import *
--- koolculator/numculator/modulo.py
+++ koolculator/numculator/modulo.py
@@ -48,3 +48,45 @@ def gcd_extended_euclid(a, b):
 	else:
 		return -a, -mpp, -npp
 
+def chinese_remainder(m, X):
+	'''
+	Given two lists of integers m = [m1, m2, ..., mn] and
+	X = [x1, x2, ..., xn] of same length, and assuming the 
+	integers in m are relatively prime, this function returns
+	the solution of the system of equations
+	x === x1 (mod m1)
+	x === x2 (mod m2)
+	...
+	x === xn (mod mn)
+	(where === is the congruence sign), using the Chinese 
+	Remainder Theorem. Also for each i, xi < mi must hold.
+
+	Algorithm source: Computer Algebra and Symbolic Computation:
+	Mathematical Methods by Joel S. Cohen
+
+	'''
+	if len(m) != len(X):
+		print("m and X must be equal in length")
+		return
+
+	for mi, xi in zip(m, X):
+		if xi > mi:
+			print("mi and xi must satisfy xi < mi")
+			return
+
+	n = m[0]
+	s = X[0]
+
+	for i in range(1, len(m)):
+		xi = X[i]
+		mi = m[i]
+
+		e = gcd_extended_euclid(n, mi)
+		c = e[1]
+		d = e[2]
+
+		s = c * n * xi + d * mi * s
+		n = n * mi
+
+	return s % n
+
